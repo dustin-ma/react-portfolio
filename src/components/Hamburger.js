0c4ede3bdec46a2f310161d525926a535fe02720
Dustin Ma <dustin8362@gmail.com>
2020-12-01 18:26:29 -0500

    built router + menu transitions

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,153 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import gsap from "gsap";
 
-const Hamburger = () => {
-  return <div className=''>menu</div>;
+// note that the state here is passed from Header.js!
+const Hamburger = ({ state }) => {
+  // these are the vars for our animated dom nodes
+  let menu = useRef(null);
+  let revealMenu = useRef(null);
+  let revealMenuBackground = useRef(null);
+  let revealCityBackground = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+  // let info = useRef(null);
+
+  useEffect(() => {
+    if (state.clicked === false) {
+      // close the menu
+      gsap.to([revealMenu, revealMenuBackground], {
+        duration: 0.8,
+        height: 0,
+        ease: "power3.inOut",
+        stagger: {
+          amount: 0.07,
+        },
+      });
+
+      gsap.to(menu, {
+        //change the display var to none after the animation is over
+        duration: 1,
+        css: { display: "none" },
+      });
+    } else if (
+      state.clicked === true ||
+      (state.clicked === true && state.initial === null)
+    ) {
+      // open menu
+      gsap.to(menu, {
+        //change the display var to block after the animation is over
+        duration: 0,
+        css: { display: "block" },
+      });
+      gsap.to([revealMenuBackground, revealMenu], {
+        duration: 0,
+        opacity: 1,
+        height: "100%",
+      });
+      staggerReveal(revealMenuBackground, revealMenu);
+      /* fadeInUp(); */
+      staggerText(line1, line2, line3);
+    }
+  }, [state]);
+
+  const staggerReveal = (node1, node2) => {
+    gsap.from([node1, node2], {
+      duration: 0.8,
+      height: 0,
+      transformOrigin: "right top",
+      skewY: 2,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.1,
+      },
+    });
+  };
+
+  const staggerText = (node1, node2, node3) => {
+    gsap.from([node1, node2, node3], {
+      duration: 0.8,
+      y: 100,
+      delay: 0.1,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.3,
+      },
+    });
+  };
+
+  const fadeInUp = (node) => {
+    gsap.from(node, {
+      y: 60,
+      duration: 1,
+      delay: 0.2,
+      opacity: 0,
+      ease: "power3.inOut",
+    });
+  };
+
+  return (
+    <div ref={(el) => (menu = el)} className="hamburger-menu">
+      <div
+        ref={(el) => (revealMenuBackground = el)}
+        className="menu-secondary-background-color"
+      ></div>
+      <div ref={(el) => (revealMenu = el)} className="menu-layer">
+        <div
+          ref={(el) => (revealCityBackground = el)}
+          className="menu-city-background"
+        ></div>
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
+              <nav>
+                <ul>
+                  <li>
+                    <Link ref={(el) => (line1 = el)} to="/opportunities">
+                      Opportunities
+                    </Link>
+                  </li>
+                  <li>
+                    <Link ref={(el) => (line2 = el)} to="/solutions">
+                      Solutions
+                    </Link>
+                  </li>
+                  <li>
+                    <Link ref={(el) => (line3 = el)} to="/contact-us">
+                      Contact Me
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              {/* <div className="info">
+                <h3>Sample Title</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div> */}
+              {/* <div className="locations">
+                Locations:
+                <span>Holder1</span>
+                <span>Holder2</span>
+                <span>Holder3</span>
+                <span>Holder4</span>
+                <span>Holder5</span>
+                <span>Holder6</span>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hamburger;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { withRouter, Link } from "react-router-dom";
+import Hamburger from "./Hamburger.js";
 
-const Header = () => {
-  return <header></header>;
+const Header = ({ history }) => {
+  // state for menu button
+  const [state, setState] = useState({
+    initial: false, // set the default state to false
+    clicked: null, // indicates whether the menu botton is clicked or not
+    menuName: "Menu", // this is what will display over the menu button, it will change to "Close" if clicked = true
+  });
+
+  // state for disabled menu button
+  const [disabled, setDisabled] = useState(false);
+
+  // use effect for page changes
+  useEffect(() => {
+    //listen for page change (url change in this case)
+    history.listen(() => {
+      setState({ clicked: false, menuName: "Menu" });
+    });
+  });
+
+  // handle function that changes the state for menu control
+  const handleMenu = () => {
+    disableMenu(); // disable the menu from being clicked to let the animation load first
+    if (state.initial === false) {
+      // opening menu for the first time
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: "Close",
+      });
+      console.log("opening menu for the first time");
+    } else if (state.clicked === true) {
+      // menu: opened => closed
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+
+      console.log("menu: opened => closed");
+    } else if (state.clicked === false) {
+      // menu: closed => opened
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+      console.log("closed => opened");
+    }
+  };
+
+  // Function to determin if menu button is ready to be clicked again
+  const disableMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 1200);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link to="/">DUSTINM.</Link>
+            </div>
+            <div className="menu">
+              <button disabled={disabled} onClick={handleMenu}>
+                {state.menuName}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Hamburger state={state} />
+    </header>
+  );
 };
 
-export default Header;
+export default withRouter(Header);
