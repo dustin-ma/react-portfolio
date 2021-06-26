1f1b9d14774b4f69514b7c9da91937ab8d9544a2
Dustin Ma <45908717+masoryn@users.noreply.github.com>
2021-06-26 13:36:27 -0400

    fixed styling and added nav button

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function AboutMe() {
             I am also seeking internship opportunities with a company that I can 
             grow & learn with. Please feel free to <Link className="link" to="/contact">reach out</Link> to me.
           </h3><br/>
-          <h3>Dustin</h3>
+          <h3><Link className="link" to="/">go back</Link></h3>
     </div>
     );
 }
@@ -48,13 +48,26 @@ function AboutMe() {
 function Projects() {
   return (
     <div className="infoBox fade-in-delay">
+      
 
+      <h3><Link className="link" to="/">go back</Link></h3>
     </div>
   );
 }
 
 function Contact() {
-  return <p>Feel free to reach me!</p>;
+  return (
+    <div className="infoBox fade-in-delay">
+      <h5>Reaching out 📨</h5><br/>
+          <hr className="animated" align="left"></hr>
+      <h3>Feel free to reach out to me on <a href="https://www.linkedin.com/in/madustin/">LinkedIn</a>, check out my 
+      progress on <a href="https://github.com/masoryn">GitHub</a> or directly email me at <u>dustin.jpg@gmail.com!</u></h3>
+      <br/>
+      <h3>Here is a copy of my <a href="https://drive.google.com/uc?export=download&id=1Ni2Qd80STRM8kh7NkMIde_EKDZDvf0O6">resume</a> as well.</h3>
+      <br/>
+      <h3><Link className="link" to="/">go back</Link></h3>
+    </div>
+  );
 }
 
 function Home() {
