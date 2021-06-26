a634ccd6c656e44e3923b4eb04e8648550b3e299
Dustin Ma <45908717+masoryn@users.noreply.github.com>
2021-06-26 11:53:57 -0400

    fixed compiler issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import "./App.scss";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
